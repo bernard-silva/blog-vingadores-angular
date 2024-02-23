5353b9939d259f232b4fcc0fdc612effd14e6674
bernard-silva <75813997+bernard-silva@users.noreply.github.com>
2024-02-22 22:22:11 -0300

    feat: personalizado o blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "OS VINGADORES (2012)",
+    "description": "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.",
+    "photoCover":"/assets/Vingadores1.jpg"
+  },
+  {
+    "id":"2",
+    "title": "OS VINGADORES: A ERA DE ULTRON",
+    "description": "Ao tentar proteger o planeta de ameaças, Tony Stark constrói um sistema de inteligência artificial que cuidaria da paz mundial. O projeto acaba dando errado e gera o nascimento do Ultron. Com o destino da Terra em jogo, Capitão América, Homem de Ferro, Thor, Hulk, Viúva Negra e Gavião Arqueiro terão que se unir para mais uma vez salvar a raça humana da extinção.",
+    "photoCover":"/assets/Vingadores2.jpg"
+  },
+  {
+    "id":"3",
+    "title": "OS VINGADORES: GUERRA INFINITA",
+    "description": "Homem de Ferro, Capitão América, Thor, Hulk e os Vingadores se unem para combater o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a humanidade.",
+    "photoCover":"/assets/Vingadores3.jpg"
+  },
+  {
+    "id":"4",
+    "title": "OS VINGADORES: ULTIMATO",
+    "description": "Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.",
+    "photoCover":"/assets/Vingadores4.jpg"
+  }
+]
